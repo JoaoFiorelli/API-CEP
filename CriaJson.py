b6851d45007f146c4fde26e2d08d55ecd5df758c
João Pedro Fiorelli <joao.fiorelli.13@gmail.com>
2021-04-05 00:10:24 -0300

    Criando o codigo para criar arquivos json.

---
+++ CriaJson.py
@@ -0,0 +1,40 @@
+import json
+
+"""
+MODELO
+data = {
+    "CEP": cep_id
+    "Rua": rua_id
+    "Numero": num_id
+    "Complemento": complem_id
+}
+"""
+
+data = {
+    "endereco 1": {
+        "CEP": "17245-452",
+        "Rua": "Governador Lopes",
+        "Numero": 235,
+        "Complemento": "ap 56"
+    },
+    "endereco 2": {
+        "CEP": "95325-754",
+        "Rua": "Maria do ceu",
+        "Numero": 689,
+        "Complemento": "ap 23"
+    },
+    "endereco 3": {
+        "CEP": "36245-965",
+        "Rua": "Machado de assis",
+        "Numero": 364,
+        "Complemento": 0        
+    },
+}
+
+out_file = open("myfile.json", "w")
+json.dump(data, out_file)
+out_file.close()
+
+with open('myfile.json') as f:
+      endereco = json.load(f)
+      print(endereco)
--- main.py
+++ main.py
@@ -5,12 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-    
+@app.get("/cep/{cep_id}")
+def read_item(cep_id: int):
+    return {"CEP": cep_id}
+      
