9299d6b6e1be29fa3863745c63f491239c6d76f1
João Pedro Fiorelli <joao.fiorelli.13@gmail.com>
2021-05-04 19:27:32 -0300

    Tentativa v1 de implementar condições para o CEP.

--- main.py
+++ main.py
@@ -3,19 +3,43 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def validation(cep_id):
 
-@app.get("/cep/{cep_id}")
+    if len(str(cep_id)) == 8 and type(cep_id) == int:
+        status_val = 1
+        return status_val 
+    else:
+        status_val = 0
+        return status_val
+
 def read_cep(cep_id: int):
 
-    with open('myfile.json') as f:
-        endereco = json.load(f) 
-    endereco = dict(endereco) 
+    status = validation(cep_id)  
 
-    for end in endereco.values():
+    if status == 0: 
+
+        return "Digite um CEP válido"
+
+    else:
+
+        with open('myfile.json') as f:
+            endereco = json.load(f) 
+        endereco = dict(endereco) 
+
+        for end in endereco.values():
                         
-        if end["CEP"] == cep_id:   
+            if end["CEP"] == cep_id:   
              
-            return {"CEP": end["CEP"], "Endereço": end["Rua"], "Numero": end["Numero"], 
-                    "Complemento": end["Complemento"]}
+                return {"CEP": end["CEP"], "Endereço": end["Rua"], "Numero": end["Numero"], 
+                        "Complemento": end["Complemento"]}
             
-    return "CEP não enconrado"      
+        return "CEP não enconrado"      
+
+@app.get("/cep/{cep_id}")
+def str_or_not(cep_id):
+
+    if type(cep_id) == str:
+        return "Não é"
+
+    if type(cep_id) == int:
+        read_cep(cep_id)
