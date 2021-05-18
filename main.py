da3bdcfbdcfbe10b34eb7a363cd3e97b7119f322
João Pedro Fiorelli <joao.fiorelli.13@gmail.com>
2021-05-18 00:05:37 -0300

    Validando o CEP como número de 8 digitos, desconsiderando string

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-def validation(cep_id):
+def validation_len(cep_id):
 
-    if len(str(cep_id)) == 8 and type(cep_id) == int:
+    if len(str(cep_id)) == 8:
         status_val = 1
         return status_val 
     else:
         status_val = 0
         return status_val
 
-def read_cep(cep_id: int):
+def read_cep(cep_id):
 
-    status = validation(cep_id)  
+    status = validation_len(cep_id)  
 
     if status == 0: 
 
@@ -39,7 +39,11 @@ def read_cep(cep_id: int):
 def str_or_not(cep_id):
 
     if type(cep_id) == str:
-        return "Não é"
+        if cep_id.isnumeric() == True:
+            return read_cep(int(cep_id))
+        else:
+            return "Entre com um valor numérico"
 
     if type(cep_id) == int:
-        read_cep(cep_id)
+        return read_cep(cep_id)
+
